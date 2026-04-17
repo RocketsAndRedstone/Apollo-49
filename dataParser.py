db92637b158117816b6c1dfc0e5b93dbe8db6edd
Devon Lethgo <dwlethgo@gmail.com>
2026-04-16 21:08:30 -0400

    syntax clean up

--- dataParser.py
+++ dataParser.py
@@ -6,40 +6,39 @@ def main():
     barometerFile = open("./data/testing/barometer.txt", "w")
     tempatureFile = open("./data/testing/tempature.txt", "w")
 
-    timeStamp:float
-    data:float
-    dataLine:int = 0
+    lineData:float
+    lineNumber:int = 0
 
     
     for line in masterFile:
-        match dataLine:
+        lineData = line[:-1]
+        match lineNumber:
 
             case 0:
-                timeStamp = line[:-1]
-                accelFile.write(f"{timeStamp}, ")
-                gyroFile.write(f"{timeStamp}, ")
-                barometerFile.write(f"{timeStamp}, ")
-                tempatureFile.write(f"{timeStamp}, ")
+                accelFile.write(f"{lineData}, ")
+                gyroFile.write(f"{lineData}, ")
+                barometerFile.write(f"{lineData}, ")
+                tempatureFile.write(f"{lineData}, ")
             case 1:
-                accelFile.write(f"{line[:-1]}, ")
+                accelFile.write(f"{lineData}, ")
             case 2:
-                accelFile.write(f"{line[:-1]}, ")
+                accelFile.write(f"{lineData}, ")
             case 3:
-                accelFile.write(f"{line[:-1]}")
+                accelFile.write(f"{lineData}")
             case 4:
-                gyroFile.write(f"{line[:-1]}, ")
+                gyroFile.write(f"{lineData}, ")
             case 5:
-                gyroFile.write(f"{line[:-1]}, ")
+                gyroFile.write(f"{lineData}, ")
             case 6:
-                gyroFile.write(f"{line[:-1]}")
+                gyroFile.write(f"{lineData}")
             case 7:
-                barometerFile.write(f"{line[:-1]}, ")
+                barometerFile.write(f"{lineData}, ")
             case 8:
-                barometerFile.write(f"{line[:-1]}")
+                barometerFile.write(f"{lineData}")
             case 9:
-                tempatureFile.write(f"{line[:-1]}, ")
+                tempatureFile.write(f"{lineData}, ")
             case 10:
-                tempatureFile.write(f"{line[:-1]}")
+                tempatureFile.write(f"{lineData}")
             case 11:
                 dataLine = 0
                 accelFile.write("\n")
@@ -48,7 +47,7 @@ def main():
                 tempatureFile.write("\n")
                 continue
 
-        dataLine += 1
+        lineNumber += 1
 
 
     masterFile.close()
