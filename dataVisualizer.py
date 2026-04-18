8a5d4bd315addd374d5a602caaa64f38f316241d
Devon Lethgo <dwlethgo@gmail.com>
2026-04-18 17:49:13 -0400

    Added method to remove gravity from acceleration messurments to ensure only aplied acceleration is being ploted

--- dataVisualizer.py
+++ dataVisualizer.py
@@ -1,17 +1,14 @@
 from matplotlib import animation, pyplot as plot
-from math import tan, arctan, pi
+from math import tan, atan, tau
 
+GRAVITY = -9.81
 
 def main():
     #Open files
     gyroscopeFile = open("./data/testing/gyroscope.txt")
     accelerationFile = open("./data/testing/acceleration.txt")
 
-    GRAVITY = 9.81
-
-    orientation:list[float] = [0, 0, 0] #yaw, pitch, roll in radian
-
-    time_stamp:list[float] = []
+    timeStamp:list[float] = []
 
     gyro_X:list[float] = []
     gyro_Y:list[float] = []
@@ -25,40 +22,38 @@ def main():
     for line in accelerationFile:
         splitLine = line.split(",")
         #milliseconds to seconds
-        time_stamp.append(float(splitLine[0]) / 1000)
-        accel_X.append(float(splitLine[1][1:-1]))
-        accel_Y.append(float(splitLine[2][1:-1]))
-        accel_Z.append(float(splitLine[3][1:-1]))
+        timeStamp.append(float(splitLine[0]) / 1000)
+        accel_X.append(float(splitLine[1]))
+        accel_Y.append(float(splitLine[2]))
+        accel_Z.append(float(splitLine[3]))
 
     for line in gyroscopeFile:
         splitLine = line.split(",")
-        gyro_X.append(float(splitLine[1][1:-1]))
-        gyro_Y.append(float(splitLine[1][1:-1]))
-        gyro_Z.append(float(splitLine[1][1:-1]))
+        gyro_X.append(float(splitLine[1]))
+        gyro_Y.append(float(splitLine[1]))
+        gyro_Z.append(float(splitLine[1]))
 
     #Close files
     gyroscopeFile.close()
     accelerationFile.close()
 
-    #Determine inital orientation
-    orientation[0] = arctan(GRAVITY / accel_X)
-    orientation[1] = arctan(GRAVITY / accel_Y)
-    orientation[2] = arctan(GRAVITY / accel_Z)
+    accel_X = removeGravity(gyro_X, accel_X, timeStamp)
+    accel_Y = removeGravity(gyro_Y, accel_Y, timeStamp)
+    accel_Z = removeGravity(gyro_Z, accel_Z, timeStamp)
+    #first position only used to determin inital orientation and does not get corrected
+    del accel_X[0], accel_Y[0], accel_Z[0], timeStamp[0]
 
-    velocity_X:list[float] = intergrateVelocity(time_stamp, accel_X)
-    velocity_Y:list[float] = intergrateVelocity(time_stamp, accel_Y)
-    velocity_Z:list[float] = intergrateVelocity(time_stamp, accel_Z)
+    velocity_X:list[float] = intergrateVelocity(timeStamp, accel_X)
+    velocity_Y:list[float] = intergrateVelocity(timeStamp, accel_Y)
+    velocity_Z:list[float] = intergrateVelocity(timeStamp, accel_Z)
 
-    position_X:list[float] = intergratePosition(time_stamp, accel_X, velocity_X)
-    position_Y:list[float] = intergratePosition(time_stamp, accel_Y, velocity_Y)
-    position_Z:list[float] = intergratePosition(time_stamp, accel_Z, velocity_Z)
+    position_X:list[float] = intergratePosition(timeStamp, accel_X, velocity_X)
+    position_Y:list[float] = intergratePosition(timeStamp, accel_Y, velocity_Y)
+    position_Z:list[float] = intergratePosition(timeStamp, accel_Z, velocity_Z)
 
     visualizeData(accel_X, accel_Y, accel_Z, "Acceleration")
     visualizeData(velocity_X, velocity_Y, velocity_Z, "Velocity")
     visualizeData(position_X, position_Y, position_Z, "Position")
-
-    
-
     
 
 def visualizeData(data_X:list[float], data_Y:list[float], data_Z:list[float], name:str) -> None:
@@ -79,29 +74,44 @@ def visualizeData(data_X:list[float], data_Y:list[float], data_Z:list[float], na
     plot.show()
 
 #v_f = v_i + (a * t)
-def intergrateVelocity(time_stamp:list[float], values:list[float]) -> list[float]:
-    timeLast:float = time_stamp[0]
+def intergrateVelocity(timeStamp:list[float], values:list[float]) -> list[float]:
+    timeLast:float = timeStamp[0]
     velocityLast:float = 0
     updatedValues:list[float] = []
-    for t in range(1, len(time_stamp)):
-        deltaTime = time_stamp[t] - timeLast
+    for t in range(1, len(timeStamp)):
+        deltaTime = timeStamp[t] - timeLast
         velocityLast += deltaTime * values[t]
         updatedValues.append(velocityLast)
-        timeLast = time_stamp[t]
+        timeLast = timeStamp[t]
 
     return updatedValues
 
 #delta x = (v_i * t) + (0.5 * a * t^2)
-def intergratePosition(time_stamp:list[float], acceleration:list[float], velocity:list[float]) -> list[float]:
-    timeLast:float = time_stamp[0]
+def intergratePosition(timeStamp:list[float], acceleration:list[float], velocity:list[float]) -> list[float]:
+    timeLast:float = timeStamp[0]
     
     newPositions:list[float] = []
-    for t in range(1, len(time_stamp)):
-        deltaTime = time_stamp[t] - timeLast
+    for t in range(1, len(timeStamp)):
+        deltaTime = timeStamp[t] - timeLast
         newPositions.append((velocity[t-1] * timeLast) + (0.5 * acceleration[t-1] * (deltaTime * deltaTime)))
-        timeLast = time_stamp[t]
+        timeLast = timeStamp[t]
 
     return newPositions
+
+def removeGravity(gyro:list[float], accel:list[float], timeStamp:list[float]):
+    theta:float = atan(GRAVITY / accel[0])
+    deltaTime:float = 0
+    timeLast:float = timeStamp[0]
+
+    for i in range(1, len(accel)):
+        deltaTime = timeStamp[i] - timeLast
+        accel[i] = accel[i] - (GRAVITY / tan(theta))
+        theta += gyro[i] * deltaTime
+        if (theta > tau or theta < -tau):
+            theta = theta % tau
+        timeLast = timeStamp[i]    
+
+    return accel
 
 if (__name__ == "__main__"):
     main()
